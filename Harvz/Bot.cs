0a53a4f45b561b6dac4bef9dedc6d2e7421265b6
Shaw <certain@coffos.space>
2020-12-04 13:55:30 +0100

    Initialize bot finished

--- Harvz/Bot.cs
+++ Harvz/Bot.cs
@@ -1,7 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
 
 namespace Harvz
 {
+    /// <summary>
+    /// Bot entry structure.
+    /// </summary>
     internal struct Bot
     {
         #region Variables
@@ -14,14 +21,23 @@ namespace Harvz
         /// File name for the config.
         /// </summary>
         private const string config = "config.json";
+
+        /// <summary>
+        /// A <see cref="DiscordSocketClient"/> instance, created at <see cref="InitializeBot"/>.
+        /// </summary>
+        private DiscordSocketClient client;
         #endregion
 
         /// <summary>
         /// Setup the bot.
         /// </summary>
-        public static void Main()
+        public static async Task Main()
         {
             instance.InitializeFiles();
+            // ReSharper disable once PossiblyImpureMethodCallOnReadonlyVariable
+            await instance.InitializeBot();
+            await Task.Delay(-1); // ! << Never exit
+            instance.client.Dispose();
         }
 
         /// <summary>
@@ -38,6 +54,19 @@ namespace Harvz
             if (File.Exists(config)) return;
             File.Create(config).Close();
             File.WriteAllText(config, "{\"token\": \"Your token here.\"}");
+        }
+
+        /// <summary>
+        /// Initialize the bot.
+        /// </summary>
+        private async Task InitializeBot()
+        {
+            Console.WriteLine("<!> Bot starting");
+            var content = JObject.Parse(File.ReadAllText(config));
+            client = new DiscordSocketClient();
+            await client.LoginAsync(0, content["token"].ToString());
+            await client.StartAsync();
+            Console.WriteLine("<!> Bot started!");
         }
     }
 }
